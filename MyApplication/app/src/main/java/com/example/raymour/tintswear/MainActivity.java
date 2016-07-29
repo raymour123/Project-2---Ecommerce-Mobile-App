d6e9cc1148a32619cc1923baa24a36ab6fc342f2
Raymour <raymour@hotmail.com>
2016-07-29 15:11:48 -0400

    Added a splash screen but isn't functional yet

--- MyApplication/app/src/main/java/com/example/raymour/tintswear/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/raymour/tintswear/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -147,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -158,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.search) {
             return true;
         }
 
