dd4b297aa1d5297870d1715f2ec2a0e79cecc959
Raymour <raymour@hotmail.com>
2016-08-01 06:23:38 -0400

    Splash Screen and some Aesthetics

--- MyApplication/app/src/main/java/com/example/raymour/tintswear/FullscreenActivity.java
+++ MyApplication/app/src/main/java/com/example/raymour/tintswear/FullscreenActivity.java
@@ -1,12 +1,14 @@
 package com.example.raymour.tintswear;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -106,6 +108,19 @@ public class FullscreenActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        Button button = (Button) findViewById(R.id.dummy_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FullscreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
 
 
